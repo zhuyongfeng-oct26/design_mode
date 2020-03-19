667649edb0834770e0a2185a1a1a1b5b29a4504e
bamboo <bamboo@192.168.0.102>
2020-03-19 22:00:40 +0800

    test

---
+++ php/Singleton.php
@@ -0,0 +1,39 @@
+<?php
+
+class Singleton {
+
+	//创建静态私有变量保存对象
+	static private $instance;
+
+	//参数
+	public $name;
+
+	//防止直接创建对象
+	private function __construct($name) {
+		$this->name = $name;
+	}
+
+	//防止克隆
+	private function __clone() {}
+
+
+	static public function getInstance($name) {
+		if(!self::$instance instanceof self) {
+			self::$instance = new self($name);
+		}
+
+		return self::$instance;
+	}
+
+	public function test() {
+		echo '我是一个 ' . $this->name; 
+	}
+}
+
+$obj = Singleton::getInstance('单例类1');
+$obj->test();
+
+
+$obj = Singleton::getInstance('单例类2');
+$obj->test();
+
