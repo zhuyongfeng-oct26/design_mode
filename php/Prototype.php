9a804b7227ff7a23a41aaf91e3eef1af992c6ef2
bamboo <bamboo@192.168.0.102>
2020-03-24 20:19:30 +0800

    原型设计模式

---
+++ php/Prototype.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * 原型模式
+*/
+
+abstract class Prototype {
+    abstract function cloned();
+}
+
+class Demo extends Prototype {
+
+    public $title = '';
+
+    public function getTitle()
+    {
+        echo $this->title . PHP_EOL;
+    }
+
+    public function cloned()
+    {
+        return clone $this;
+    }
+}
+
+$obj_1 = new Demo();
+$obj_1->title = 'test demo 1';
+$obj_1->getTitle();
+
+$obj_2 = $obj_1->cloned();
+$obj_2->getTitle();
